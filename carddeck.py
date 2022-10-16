bb47f0c4862d53f864ce2409da3fa5da6891446f
Andre Arnold <115298503+akarnold726@users.noreply.github.com>
2022-10-16 12:51:17 -0700

    Add files via upload

---
+++ carddeck.py
@@ -0,0 +1,359 @@
+standard_card_deck = [
+    {
+        "id": 0,
+        "name": "2 of Spades",
+        "suit": "Spades",
+        "value": 2,
+        "color": "0"
+    },
+    {
+        "id": 1,
+        "name": "3 of Spades",
+        "suit": "Spades",
+        "value": 3,
+        "color": "0"
+    },
+    {
+        "id": 2, 
+        "name": "4 of Spades",
+        "suit":"Spades",
+        "value": 4,
+        "color": "0"   
+    },
+    {
+        "id": 3,
+        "name": "5 of Spades",
+        "suit": "Spades",
+        "value": 5,
+        "color": "0"
+    },
+    {
+        "id": 4,
+        "name": "6 of Spades",
+        "suit": "Spades",
+        "value": 6,
+        "color": "0"
+    },
+    {
+        "id": 5,
+        "name": "7 of Spades",
+        "suit": "Spades",
+        "value": 7,
+        "color": "0"
+    },
+    {
+        "id; 6",
+        "name": "8 of Spades",
+        "suit": "Spades",
+        "value": 8,
+        "color": "0"
+    },
+    {
+        "id": 7,
+        "name": "9 of Spades",
+        "suit": "Spades",
+        "value": 9,
+        "color": "0"
+    },
+    {
+        "id": 8,
+        "name": "10 of Spades",
+        "suit": "Spades",
+        "value": 10,
+        "color": "0"
+    },
+    {
+        "id": 9,
+        "name": "Jack of Spades",
+        "suit": "Spades",
+        "value": 11,
+        "color": "0"
+    },
+    {
+        "id":10,
+        "name": "Queen of Spades",
+        "suit": "Spades",
+        "value": 12,
+        "color": "0"
+    },
+    {
+        "id": 11,
+        "name": "King of Spades",
+        "suit": "Spades",
+        "value": 13,
+        "color": "0"
+    },
+    {
+        "id": 12,
+        "name": "Ace of Spades",
+        "suit": "Spades",
+        "value": 14,
+        "color": "0"
+    },
+    {
+        "id": 13,
+        "name": "2 of Hearts",
+        "suit": "Hearts",
+        "value": 2,
+        "color": "1"
+    },
+    {
+        "id": 14,
+        "name": "3 of Hearts",
+        "suit": "Hearts",
+        "value": 3,
+        "color": "1"
+    },
+    {
+        "id": 15,
+        "name": "4 of Hearts",
+        "suit": "Hearts",
+        "value": 4,
+        "color": "1"
+    },
+    {
+        "id": 16,
+        "name": "5 of Hearts",
+        "suit": "Hearts",
+        "value": 5,
+        "color": "1"
+    },
+    {
+        "id"= 17,
+        "name": "6 of Hearts",
+        "suit": "Hearts",
+        "value": 6,
+        "color": "1"
+    },
+    {
+        "id": 18,
+        "name": "7 of Hearts",
+        "suit": "Hearts",
+        "value": 7,
+        "color": "1"
+    },
+    {
+        "id": 19,
+        "name": "8 of Hearts",
+        "suit": "Hearts",
+        "value": 8,
+        "color": "1"
+    },
+    {
+    "id": 20,
+    "name": "9 of Hearts",
+    "suit": "Hearts",
+    "value": 9,
+    "color": "1"
+    },
+    {
+        "id": 21,
+        "name": "10 of Hearts",
+        "suit": "Hearts",
+        "value": 10,
+        "color": "1"
+    },
+    {
+        "id": 22,
+        "name": "Jack of Hearts",
+        "suit": "Hearts",
+        "value": 11,
+        "color": "1"
+    },
+    {
+        "id": 23,
+        "name": "Queen of Hearts",
+        "suit": "Hearts",
+        "value": 11,
+        "color": "1"
+    },
+    {
+        "id": 24,
+        "name": "King of Hearts",
+        "suit": "Hearts",
+        "value": 14,
+        "color": "1"
+    },
+    {
+        "id": 25,
+        "name": "Ace of Hearts",
+        "suit": "Hearts",
+        "value": 14,
+        "color": "1"
+    },
+    {
+        "id": 26,
+        "name": "2 of Clubs",
+        "suit": "Clubs",
+        "value": 2,
+        "color": "0"
+    },
+    {
+        "id": 27,
+        "name": "3 of Clubs",
+        "suit": "Clubs",
+        "value": 3,
+        "color": "0"
+    },
+    {
+        "id": 28,
+        "name": "4 of Clubs",
+        "suit": "Clubs",
+        "value": 4,
+        "color": "0"
+    },
+    {
+        "id": 29,
+        "name": "5 of Clubs",
+        "suit": "Clubs",
+        "value": 5,
+        "color": "0"
+    },
+    {
+        "id": 30,
+        "name": "6 of Clubs",
+        "suit": "Clubs",
+        "value": 6,
+        "color": "0"
+    },
+    {
+        "id": 31,
+        "name": "7 of Clubs",
+        "suit": "Clubs",
+        "value": 7, 
+        "color": "0"
+    },
+    {
+        "id": 32,
+        "name": "8 of Clubs",
+        "suit": "Clubs",
+        "value": 8,
+        "color": "0"
+    },
+    {
+        "id": 33,
+        "name": "9 of Clubs",
+        "suit": "Clubs",
+        "value": 9,
+        "color":"0"
+    },
+    {
+        "id": 34,
+        "name": "10 of Clubs",
+        "suit": "Clubs",
+        "value": 10,
+        "color": "0"
+    },
+    {
+        "id": 35,
+        "name": "Queen of Clubs",
+        "suit": "Clubs",
+        "value": 12,
+        "color": "0"
+    },
+    {
+        "id": 37,
+        "name": "King of Clubs",
+        "suit": "Clubs",
+        "value": 13,
+        "color": "0"
+    },
+    {
+        "id": 38,
+        "name": "Ace of Clubs",
+        "suit": "Clubs",
+        "value": 14,
+        "color": "0"
+    },
+    {
+        "id": 39,
+        "name": "2 of Diamonds",
+        "suit": "Diamonds",
+        "value": 2,
+        "color": "1"
+    },
+    {
+        "id": 40,
+        "name": "3 of Diamonds",
+        "suit": "Diamonds",
+        "value": 3,
+        "color": "1"
+    },
+    {
+        "id": 41,
+        "name": "4 of Diamonds",
+        "suit": "Diamonds",
+        "value": 4,
+        "color": "1"
+    },
+    {
+        "id": 42,
+        "name": "5 of Diamonds",
+        "suit": "Diamonds",
+        "value": 5,
+        "color": "1"
+    },
+    {
+        "id": 43,
+        "name": "6 of Diamonds",
+        "suit": "Diamonds",
+        "value": 6,
+        "color": "1"
+    },
+    {
+        "id": 44,
+        "name": "7 of Diamonds",
+        "suit": "Diamonds",
+        "value": 7,
+        "color": "1"
+    },
+    {
+        "id": 45,
+        "name": "8 of Diamonds",
+        "suit": "Diamonds",
+        "value": 8,
+        "color": "1"
+    },
+    {
+        "id": 46,
+        "name": "9 of Diamonds",
+        "suit": "Diamonds",
+        "value": 9,
+        "color": "1"
+    },
+    {
+        "id": 47,
+        "name": "10 of Diamonds",
+        "suit": "Diamonds",
+        "value": 10,
+        "color": "1"
+    },
+    {
+        "id": 48, 
+        "name": "Jack of Diamonds",
+        "suit": "Diamonds",
+        "value": 10,
+        "color": "1"
+    },
+    {
+        "id": 49,
+        "name": "Queen of Diamonds",
+        "suit": "Diamonds",
+        "value": 12,
+        "color": "1"
+    },
+    {
+        "id": 50,
+        "name": "King of Diamonds",
+        "suit": "Diamonds",
+        "value": 13,
+        "color": "1"
+    },
+    {
+        "id": 51,
+        "name": "Ace of Diamonds",
+        "suit": "Diamonds",
+        "value": 14,
+        "color": "1"
+    }
+]
---
+++ play_war.py
@@ -0,0 +1,74 @@
+import deal_cards
+import carddeck
+
+def play_war(): 
+    player_cards = deal_cards.deal_cards(2, 26, carddeck.standard_card_deck)[0]
+    p_deck = player_cards["player0"]
+    o_deck = player_cards["player1"]
+    round_number = 0
+    end_message = "time out"
+    played_card_log = []
+    win_state = False 
+    while round_number < 10000 and win_state is False:
+        cards_in_play = []
+        round_number += 1
+        print("Round" + str(round_number))
+        cards_in_play.append(p_deck[0])
+        print("You played" + p_deck[0]["name"])
+        cards_in_play.append(o_deck[0])
+        print("Oppenent played" + o_deck[0]["name"])
+        p_deck = p_deck[1:]
+        o_deck = o_deck[1:]
+        if cards_in_play[0]["value"] == cards_in_play[1]["value"]:
+            war_state = True 
+            while war_state is True:
+                print("war!")
+                if len(p_deck) < 3 or len(o_deck) < 3:
+                    if len(p_deck) < 3 and len(o_deck) < 3:
+                        end_message = "Tie game, both players ran out of cards"
+                    if len(p_deck) < 3:
+                        end_message = "You lose the game, you ran out of cards"
+                    if len(o_deck) < 3: 
+                        end_message = "You win the game, oppenent ran out of cards"
+                    war_state = False
+                    win_state = True 
+                else: 
+                    cards_in_play += p_deck[0:3]
+                    for x in p_deck[0:3]:
+                        print("You played" + x["name"])
+                    cards_in_play += o_deck[0:3]
+                    for x in o_deck{0:3}:
+                        print("Opponent playeed" + x["name"])
+                    p_deck = p_deck[3:]
+                    o_deck = o_deck[3:]
+                    if cards_in_play[-1]["value"] != cards_in_play[-4]["value"]:
+                        if cards_in_play[-1]["value"] != cards_in_play[-4]["value"]:
+                            print("Opponent won the war!")
+                            o_deck = o_deck + cards_in_play
+                        else: 
+                            print("You won the war!")
+                            p_deck = p_deck + cards_in_play
+                        war_state = False 
+                else:
+                    if cards_in_play[0]["value"] > cards_in_play[1]["value"]:
+                        p_deck = p_deck + cards_in_play 
+                        print("you won the round!")
+                else: 
+                    o_deck = o_deck + cards_in_play 
+                    print ("your opponent won the round!")
+                if len(p_deck) == 0:
+                    end_message = "you lost the game :("
+                    win_state = True 
+                if len(o_deck) == 0:
+                    end_message = "you win the game :) Congrats!!"
+                    win_state = True 
+                this_rounds_cards = []
+                for x in cards_in_play: 
+                    this_rounds_cards.append([x["name"]])
+                played_card_log.append(this_rounds_cards)
+                p_deck = deal_cards.deal_cards(1, len(p_deck), p_deck[0]["player0"]
+                o_deck = deal_cards.deal_cards(1, len(o_deck), o_deck)[0]["player0"]
+                
+            return (end_message)
+        
+print(play_war())
